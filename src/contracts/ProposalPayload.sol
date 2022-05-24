ec0fb04b6672e05bfd2de218b3ed967ac88067c1
sendra <polsendra@gmail.com>
2022-05-24 16:55:32 +0200

    fix: fixed some types

--- src/contracts/ProposalPayload.sol
+++ src/contracts/ProposalPayload.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
-import {IAaveMerkleDistributor} from './interfaces/IAaveMerkleDistributor';
-import {AaveMerkleDistributor} from './AaveMerkleDistributor';
+import {IAaveMerkleDistributor} from './interfaces/IAaveMerkleDistributor.sol';
+import {AaveMerkleDistributor} from './AaveMerkleDistributor.sol';
 
 /// @title Payload to initialize the tokens rescue phase 1
 /// @author BGD
@@ -12,20 +12,20 @@ import {AaveMerkleDistributor} from './AaveMerkleDistributor';
 ///         - AAVE, stkAAVE, USDT, UNI tokens
 contract ProposalPayload {
     // AAVE distribution
-    address public constant AAVE_TOKEN = 0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9;
-    address public constant AAVE_MERKLE_ROOT = 0xc2b53b6e06509b53a9ce00ce0ab1955b9dcf607774c46e7268ee1c990436003f;
+    address public constant AAVE_TOKEN = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
+    bytes32 public constant AAVE_MERKLE_ROOT = 0xc2b53b6e06509b53a9ce00ce0ab1955b9dcf607774c46e7268ee1c990436003f;
 
     // stkAAVE distribution
-    address public constant stkAAVE_TOKEN = 0x4da27a545c0c5b758a6ba100e3a049001de870f5;
-    address public constant stkAAVE_MERKLE_ROOT = 0x71d2b70cb25ea6bbdc276c4b4b9f209c53131d652f962b4d5f6d89fe5a1c6760;
+    address public constant stkAAVE_TOKEN = 0x4da27a545c0c5B758a6BA100e3a049001de870f5;
+    bytes32 public constant stkAAVE_MERKLE_ROOT = 0x71d2b70cb25ea6bbdc276c4b4b9f209c53131d652f962b4d5f6d89fe5a1c6760;
 
     // USDT distribution
     address public constant USDT_TOKEN = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-    address public constant USDT_MERKLE_ROOT = 0xc7ee13da36bc0398f570e2c50daea6d04645f112371489486655d566c141c156;
+    bytes32 public constant USDT_MERKLE_ROOT = 0xc7ee13da36bc0398f570e2c50daea6d04645f112371489486655d566c141c156;
 
     // UNI distribution
     address public constant UNI_TOKEN = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
-    address public constant UNI_MERKLE_ROOT = 0x0d02ecdaab34b26ed6ffa029ffa15bc377852ba0dc0e2ce18927d554ea3d939e;
+    bytes32 public constant UNI_MERKLE_ROOT = 0x0d02ecdaab34b26ed6ffa029ffa15bc377852ba0dc0e2ce18927d554ea3d939e;
 
     // short executor
     address public constant SHORT_EXECUTOR = 0xEE56e2B3D491590B5b31738cC34d5232F378a8D5;
