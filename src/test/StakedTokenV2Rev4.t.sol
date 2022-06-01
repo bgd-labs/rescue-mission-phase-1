de8ae76f5dc16eb7a929208a8d4b9b199d8399b7
sendra <polsendra@gmail.com>
2022-06-01 15:18:40 +0200

    fix: updated test for stk aave

--- src/test/StakedTokenV2Rev4.t.sol
+++ src/test/StakedTokenV2Rev4.t.sol
@@ -2,16 +2,15 @@
 pragma solidity 0.7.5;
 
 import "forge-std/Test.sol";
-// import {IERC20} from "../contracts/dependencies/openZeppelin/pre-v8/IERC20.sol";
-// import {SafeERC20} from "../contracts/dependencies/openZeppelin/pre-v8/SafeERC20.sol";
 import {IInitializableAdminUpgradeabilityProxy} from "../contracts/interfaces/IInitializableAdminUpgradeabilityProxy.sol";
-import {StakedTokenV2Rev4, IERC20, SafeERC20} from "../contracts/StakedTokenV2Rev4.sol";
+import {StakedTokenV2Rev4, IERC20, SafeERC20, SafeMath} from "../contracts/StakedTokenV2Rev4.sol";
   
 
 contract StakedTokenV2Rev4Test is Test {
     using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
-    address public constant AAVE_MERKLE_DISTRIBUTOR = address(1);
+    address public constant AAVE_MERKLE_DISTRIBUTOR = address(124312);
     address public constant AAVE_LONG_EXECUTOR = 0x61910EcD7e8e942136CE7Fe7943f956cea1CC2f7;
     
     address public constant AAVE_TOKEN = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
@@ -30,6 +29,8 @@ contract StakedTokenV2Rev4Test is Test {
     string public constant name = "Staked Aave";
     string public constant symbol = "stkAAVE";
     uint8 public constant decimals = 18;
+
+    event TokensRescued(address indexed tokenRescued, address indexed aaveMerkleDistributor, uint256 amountRescued);
 
     IInitializableAdminUpgradeabilityProxy public proxyStake;
     StakedTokenV2Rev4 stkAaveImpl;
@@ -62,6 +63,11 @@ contract StakedTokenV2Rev4Test is Test {
         amounts[0] = AAVE_RESCUE_AMOUNT;
         amounts[1] = STK_AAVE_RESCUE_AMOUNT;
 
+        vm.expectEmit(true, true, false, true);
+        emit TokensRescued(tokens[0], AAVE_MERKLE_DISTRIBUTOR, amounts[0]);
+        vm.expectEmit(true, true, false, true);
+        emit TokensRescued(tokens[1], AAVE_MERKLE_DISTRIBUTOR, amounts[1]);
+        
         vm.prank(AAVE_LONG_EXECUTOR);
         proxyStake.upgradeToAndCall(
             address(stkAaveImpl), 
@@ -72,5 +78,22 @@ contract StakedTokenV2Rev4Test is Test {
                 AAVE_MERKLE_DISTRIBUTOR
             )
         );
+
+        // test that is has initialized correctly
+        StakedTokenV2Rev4 stkAave = StakedTokenV2Rev4(STK_AAVE_TOKEN);
+        assertEq(stkAave.name(), name);
+        assertEq(address(stkAave.STAKED_TOKEN()), address(stakedToken));
+        assertEq(address(stkAave.REWARD_TOKEN()), address(rewardToken));
+        assertEq(stkAave.COOLDOWN_SECONDS(), cooldownSeconds);
+        assertEq(stkAave.UNSTAKE_WINDOW(), unstakeWindow);
+        assertEq(stkAave.REWARDS_VAULT(), rewardsVault);
+        assertEq(stkAave.EMISSION_MANAGER(), emissionManager);
+        assertEq(stkAave.DISTRIBUTION_END(), block.timestamp.add(distributionDuration));
+        assertEq(stkAave.symbol(), symbol);
+        console.log('decimals:', stkAave.decimals());
+        // assertEq(stkAave.decimals(), decimals);
+
+        assertEq(IERC20(AAVE_TOKEN).balanceOf(AAVE_MERKLE_DISTRIBUTOR), AAVE_RESCUE_AMOUNT);
+        assertEq(IERC20(STK_AAVE_TOKEN).balanceOf(AAVE_MERKLE_DISTRIBUTOR), STK_AAVE_RESCUE_AMOUNT);
     }
 }
