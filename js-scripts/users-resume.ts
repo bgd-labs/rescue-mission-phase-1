b015aa5d195547200c9d94045acf3e2668cec68e
sendra <polsendra@gmail.com>
2023-01-26 11:45:31 +0100

    fix: added users amounts json

--- js-scripts/users-resume.ts
+++ js-scripts/users-resume.ts
@@ -1,11 +1,13 @@
 import fs from "fs";
 
+type LightUserInfo = Record<string, Record<string, string>>;
+
 type UserInfo = {
-    tokenAmount: string;
     tokenAmountInWei: string;
     proof: string[];
     index: number;
     distributionId: number;
+    tokenAmount: string;
 }
 
 type Claim = {
@@ -52,6 +54,7 @@ const getMerkleTreeJson = (path: string): MerkleTree => {
 
 const generateUsersJson = (): void => {
     const usersJson: UsersJson = {}
+    const lightUsersJson: LightUserInfo = {};
 
     for (const token of Object.keys(merkleTree)) {
         const merkleTreeJson = getMerkleTreeJson(merkleTree[token]);
@@ -60,7 +63,14 @@ const generateUsersJson = (): void => {
                 usersJson[claimer] = [];
             }
 
+            if (!lightUsersJson[claimer]) {
+                lightUsersJson[claimer] = {};
+            }
+
             const claimerInfo = merkleTreeJson.claims[claimer];
+
+            lightUsersJson[claimer][token] = claimerInfo.amount;
+
             usersJson[claimer].push({
                 tokenAmount: claimerInfo.amount,
                 tokenAmountInWei: claimerInfo.amountInWei,
@@ -72,6 +82,7 @@ const generateUsersJson = (): void => {
     }
 
     fs.writeFileSync('./js-scripts/maps/usersMerkleTrees.json', JSON.stringify(usersJson));
+    fs.writeFileSync('./js-scripts/maps/usersAmounts.json', JSON.stringify(lightUsersJson));
 }
 
 generateUsersJson();
